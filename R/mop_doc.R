e2cec4758a7aec629f5598bd78a9c96bd607806f
marlonecobos <manubio13@gmail.com>
2023-04-26 23:24:29 -0500

    v0.0.3 first draft of package documentation added

---
+++ R/mop_doc.R
@@ -0,0 +1,25 @@
+#' mop: Mobility Oriented-Parity Metric
+#'
+#' @description
+#' mop is a set of tools to perform the Mobility Oriented-Parity metric which
+#' helps to compare a set of conditions of reference versus another set of
+#' conditions of interest. The main goal is to explore conditions in the set of
+#' interest that are non-analogous to those in the reference set.
+#'
+#' @details
+#' To detect levels of dissimilarity between a set of
+#' conditions of reference and another set of conditions of interest. The
+#' tools included here also help to identify conditions outside rages of the
+#' reference set with high detail.
+#'
+#' @section Functions in mop:
+#' \code{\link{mop}}, \code{\link{mop_distance}},
+#' \code{\link{out_range}}
+#'
+#' @section Data included:
+#' \code{\link{reference_matrix}}, \code{\link{matrix_of_interest}},
+#' \code{\link{reference_layers}}, \code{\link{layers_of_interest}}
+#'
+#' @docType package
+#' @name mop
+NULL
