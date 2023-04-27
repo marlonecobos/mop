6ffeedf3c08aedc2558a5219eedc9a07cc9b73be
marlonecobos <manubio13@gmail.com>
2023-04-27 16:25:00 -0500

    v0.0.6 function match_na_raster added and documented

---
+++ R/helpers.R
@@ -0,0 +1,49 @@
+#' Match NA cells for all layers in SpatRaster
+#'
+#' @description
+#' Options to identify which values in a set of conditions of interest
+#' (\code{g_matrix}) are outside the range of a set of conditions of
+#' reference (\code{m_matrix}).
+#'
+#' @usage
+#' match_na_raster(layers)
+#'
+#' @param layers a SpatRaster object containing two or more variables to be
+#' matched.
+#'
+#' @export
+#'
+#' @importFrom terra nlyr mask app
+#'
+#' @examples
+#' # data
+#' layers <- terra::rast(system.file("extdata", "reference_layers.tif",
+#'                       package = "mop"))
+#'
+#' # add NA in some places
+#' layers[20:24, 10:16][, 3] <- NA
+#' terra::plot(layers)
+#'
+#' # match NAs
+#' matched <- match_na_raster(layers)
+#' terra::plot(matched)
+
+match_na_raster <- function(layers) {
+  if (missing(layers)) {
+    stop("Argument 'layers' must be defined.")
+  }
+  if (class(layers)[1] != "SpatRaster") {
+    stop("Argument 'layers' must of class 'SpatRaster'.")
+  }
+
+  # processing
+  if (terra::nlyr(layers) <= 1) {
+    message("'layers' has only one layer, processing is not required.")
+  } else {
+    msk <- terra::app(layers, sum)
+    layers <- terra::mask(layers, msk)
+  }
+
+  # results
+  return(layers)
+}
